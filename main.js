5102230bd3ddac8c100506120a6e1c4e26645811
myusermouse <mdrahulsarkar5525@gmail.com>
2025-01-07 12:24:19 +0600

    update

---
+++ main.js
@@ -0,0 +1,12 @@
+let form = document.getElementById('form');
+    
+function clickBtn() {
+
+    form.classList.remove('hide');
+}
+function cancel() {
+    form.classList.add('hide');
+}
+function alertop() {
+    alert("Successfully now receive notification")
+}
